9e75b09535cd481820b37ea87e58df599c186165
Evan Lin <62809897+eevaain@users.noreply.github.com>
2023-02-01 16:27:13 -0500

    working computer vision model

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 #first reveal yourself publicy -> Connect to drone -> create function that will enable drone to act on vision data --> done
 
 #winch is REST api -> built on https 
-
+#API KEY: NjEzZDA4MjI0YjIzYmM0NGJkNjU5ZDlhNzZkZDEzODk4MmYzNzE3OGFiZjc4OWZhMThhYzE5MjQ=
 
 import asyncio
 import websockets
-from flyt_python import api
 
 
-async def listen_to_data():
-    async with websockets.connect("wss://app.flytnow.com:8080/guest/63da97ae26930d0013649752") as websocket:
+async def listen_to_data(uri):
+    async with websockets.connect(uri) as websocket:
         while True:
             data = await websocket.recv()
             print(data)
 
-asyncio.run(listen_to_data())
+asyncio.run(listen_to_data("https://app.flytnow.com/guest/63da97ae26930d0013649752/dashboard"))
 
 #test
---
+++ detect.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+
+#captures live webcam or video file
+capture = cv2.VideoCapture(0)
+wh_target = 320
+confidence_threshold = 0.8
+nms_threshold = 0.3
+
+class_file = 'class.names'
+class_names = []
+
+with open(class_file, 'rt') as f:
+    class_names = f.read().rstrip('\n').split('\n')
+
+# tiny weights (higher frame rate, lower accuracies)
+#model_config = 'yolov3-tiny.cfg'
+#model_weights = 'yolov3-tiny.weights'
+
+# using more trained weights (lower frame rate, higher accuracies)
+model_config = 'yolov3.cfg'
+model_weights = 'yolov3.weights'
+
+network = cv2.dnn.readNetFromDarknet(model_config, model_weights)
+
+# use CPU
+network.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+network.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+# use GPU (CUDA)
+#network.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+#network.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+def find_objects(outputs, img):
+    hT, wT, cT = img.shape
+    bounding_box = []
+    class_ids = []
+    confidence_values = []
+
+    for output in outputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > confidence_threshold:
+                width, height = int(detection[2] * wT), int(detection[3] * hT)
+                x, y = int(detection[0] * wT - width / 2), int(detection[1] * hT - height / 2)
+                bounding_box.append([x, y, width, height])
+                class_ids.append(class_id)
+                confidence_values.append(float(confidence))
+                
+
+    indices = cv2.dnn.NMSBoxes(bounding_box, confidence_values, confidence_threshold, nms_threshold)
+
+    for i in indices:
+     
+        # okay so i think this part is where the text is outputted
+        box = bounding_box[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        cv2.rectangle(img, (x, y), (x + w, y + h), (100, 100, 255), 2)
+        cv2.putText(img, f'{class_names[class_ids[i]]} {int(confidence_values[i] * 100)}%', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (100, 100, 255), 2)
+       
+
+        if class_names[class_ids[i]] != 'person':
+            print('move foward a little bit')
+        else:
+            print('stopped')
+    
+
+
+while True:
+    success, img = capture.read()
+
+    blob = cv2.dnn.blobFromImage(img, 1 / 255, (wh_target, wh_target), [0, 0, 0], 1, crop=False)
+    network.setInput(blob)
+
+    layer_names = network.getLayerNames()
+    output_names = [layer_names[i - 1] for i in network.getUnconnectedOutLayers()]
+
+    outputs = network.forward(output_names)
+
+    find_objects(outputs, img)
+
+    cv2.imshow('Camera', img)
+    cv2.waitKey(1)
