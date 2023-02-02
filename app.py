7710ba4ca66273a20ee8e319ae732dc14d7fa52f
Jon-Fredrik Hopland <jho107@post.uit.no>
2023-02-02 14:35:39 -0500

    set up front end app, button works

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
-#first reveal yourself publicy -> Connect to drone -> create function that will enable drone to act on vision data --> done
 
-#winch is REST api -> built on https 
-#API KEY: NjEzZDA4MjI0YjIzYmM0NGJkNjU5ZDlhNzZkZDEzODk4MmYzNzE3OGFiZjc4OWZhMThhYzE5MjQ=
-
-import asyncio
-import websockets
+from flask import Flask, render_template
+from drone_api import DroneController
+from blynk_api import WinchController
+import time
 
 
-async def listen_to_data(uri):
-    async with websockets.connect(uri) as websocket:
-        while True:
-            data = await websocket.recv()
-            print(data)
+app = Flask(__name__)
 
-asyncio.run(listen_to_data("https://app.flytnow.com/guest/63da97ae26930d0013649752/dashboard"))
 
-#test
+@app.route('/', methods=['GET', 'POST']) 
+def home():
+    return render_template("index.html")
+
+@app.route('/party', methods=['GET', 'POST']) 
+def party():
+    print("clicked party")
+
+    #### FlytBase Cloud API ####
+    token = '60ac081f527e8baab8ac3bae078d1922d34cfd2e' # personal access token
+    droneHandle = DroneController(Token=token, VehicleId='r4EeYcnz',
+                              fb_server_url='https://dev.flytbase.com/rest/ros/flytos')
+
+    #### Blynk API ####
+    winch = WinchController()
+
+    winch.lower_winch()
+    print("Lowering winch...")
+    time.sleep(2)
+    winch.stop_winch()
+    time.sleep(1)
+    print("Stopped winch")
+    winch.raise_winch()
+    print("Raising winch...")
+    
+    return render_template("party.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
